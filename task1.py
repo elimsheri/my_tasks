cdcfb1b35021efda36d879b4fe497d62af9120e5
Lenar Minikaev <minikaev_lenar@.mail.ru>
2023-08-25 19:24:55 +0300

    Fixed the decision of the program through the function

--- task1.py
+++ task1.py
@@ -1,12 +1,17 @@
-n = int(input())
-a = 0
-b = 1
-if n >= a:
-    print(a)
-while n >= b:
-    print(b)
-    c = b
-    b = a + b
-    a = c
+def num_feban(n):
+    a = 0
+    b = 1
+    if n >= a:
+        print(a, end=' ')
+    while n >= b:
+        print(b, end=' ')
+        c = b
+        b = a + b
+        a = c
+print('Вывод чисел Фибоначи до указнного значения')
+n = int(input('Укажите предельное значение '))
+num_feban(n)
+
+
 
 
